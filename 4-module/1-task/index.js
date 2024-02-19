62fc8c9b98e79930953ba3631360f490ff729478
Valery Malyshev <pherum83@gmail.com>
2024-02-20 01:27:41 +0300

    4 - 1

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,14 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  const ul = document.createElement("ul");
+
+  const items = Array.from(friends).map(({ firstName, lastName }) => {
+    const li = document.createElement("li");
+    li.textContent = `${firstName} ${lastName}`;
+
+    return li;
+  });
+
+  ul.append(...items);
+
+  return ul;
 }
