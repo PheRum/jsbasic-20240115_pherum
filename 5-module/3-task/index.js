fb98ba86d4f8cd08a0f3e31595dbc887ccddfe02
Valery Malyshev <pherum83@gmail.com>
2024-02-20 03:42:18 +0300

    5 -3

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,28 @@
 function initCarousel() {
-  // ваш код...
+  const carousel = document.querySelector(".carousel__inner");
+  const width = carousel.firstElementChild.offsetWidth;
+  const countChild = carousel.childElementCount;
+  const arrowRight = document.querySelector(".carousel__arrow_right");
+  const arrowLeft = document.querySelector(".carousel__arrow_left");
+  let offset = 0;
+
+  arrowLeft.style.display = "none";
+
+  arrowRight.addEventListener("click", () => {
+    offset -= width;
+
+    arrowLeft.style.display = offset >= 0 ? "none" : "";
+    arrowRight.style.display = offset <= (countChild - 1) * width * -1 ? "none" : "";
+
+    carousel.style.transform = `translateX(${offset}px)`;
+  });
+
+  arrowLeft.addEventListener("click", () => {
+    offset += width;
+
+    arrowLeft.style.display = offset >= 0 ? "none" : "";
+    arrowRight.style.display = offset <= (countChild - 1) * width * -1 ? "none" : "";
+
+    carousel.style.transform = `translateX(${offset}px)`;
+  });
 }
